17af1410e4737b12f3713764e36b8697a3a0c19c
Sameer Adnan <samadnan9161@gmail.com>
2025-07-22 08:34:17 +0000

    auth/Configuration Not found Where can I get this?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
@@ -12,10 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-}
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 
