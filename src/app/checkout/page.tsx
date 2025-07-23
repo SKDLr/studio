796e9a51207f1c246207e56ff1a586c6a1df18c2
Sameer Adnan <samadnan9161@gmail.com>
2025-07-23 10:28:25 +0000

    there was a error placing your order

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -102,7 +102,7 @@ export default function CheckoutPage() {
       const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
       const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
       const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
-
+      
       if (serviceId && templateId && publicKey) {
         const emailParams = {
           to_name: values.name,
